92d554724d3826be50d74c601dd471d670fa6b6f
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-01-30 16:02:46 +0300

    add instruction

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 document.getElementById('count').onclick = function() {
     var number = document.getElementById('num').value;
-    document.getElementById('out').innerHTML = Math.sqrt(number);
+    document.getElementById('out').innerHTML = Math.sqrt(Math.abs(number));
+
+    if (number < 0) {
+        document.getElementById('out').innerHTML = 'please enter a positive number';
+    };
 };
